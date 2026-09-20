221d120340487d45828f35efb70700b81c94b7b5
Paulo Moreira <65466848+PauloMoreira003@users.noreply.github.com>
2026-09-20 19:23:04 +0100

    add global exception handler

---
+++ src/main/java/com/paulomoreira/task/controller/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.paulomoreira.task.controller;
+
+import com.paulomoreira.task.domain.dto.ErrorDTO;
+import com.paulomoreira.task.domain.dto.TaskDTO;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDTO> handleValidationException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream().findFirst().map(DefaultMessageSourceResolvable::getDefaultMessage).orElse(
+                "Validation Failed.");
+
+        ErrorDTO errorDTO = new ErrorDTO(errorMessage);
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+    }
+
+}
---
+++ src/main/java/com/paulomoreira/task/domain/dto/ErrorDTO.java
@@ -0,0 +1,4 @@
+package com.paulomoreira.task.domain.dto;
+
+public record ErrorDTO(String error) {
+}
